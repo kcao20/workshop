019295a033712fe2089fc3735c05451abbd5bc36
Kevin Cao <43867712+kcao20@users.noreply.github.com>
2021-09-21 09:25:23 -0400

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,7 @@
+import random
+
+pd1 = ['Patrick Ging', 'Owen Yaggy', 'Haotian Gan', 'Ishraq Mahid', 'Ivan Lam']
+pd2 = ['Kevin Cao', 'Raymond Yueng', 'Michael Borczuk', 'Yoonah Chang :)', 'Han Zhang <3', 'Austin Ngan', 'Nokai Aronesty']
+
+print("period 1: " + pd1[int(random.random() * len(pd1))])
+print("period 2: " + pd2[int(random.random() * len(pd2))])
