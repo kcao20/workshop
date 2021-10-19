aba34a8c28f80d22421e5d4ad7a157ce360996ae
kcao20 <kcao2022@gmail.com>
2021-10-19 13:15:26 -0400

    added session

--- 15_session/app.py
+++ 15_session/app.py
@@ -1,4 +1,4 @@
-# Team The Best Laptop - Jonathan W. [Loki] , Alif A. [The Eagle in the Sand], Kevin C. [Pipi] | Period 2
+# Team The Best Laptop - Jonathan Wu [Loki] , Alif Abdullah [The Eagle in the Sand], Kevin Cao [Pipi] | Period 2
 # SoftDev
 # K15 - Sessions Greetings/More form handling in HTML in flask app/We worked on creating a flask app that uses session capabilities to allow a user to login and logout.
 # 2021-10-18
@@ -6,11 +6,14 @@
 from flask import Flask             # facilitate flask webserving
 from flask import render_template   # facilitate jinja templating
 from flask import request           # facilitate form submission
+from flask import session           # facilitate session handling
 app = Flask(__name__)
+
+app.secret_key = "professional_secret_key"
 
 
 @app.route("/")  # , methods=['GET', 'POST'])
-def disp_loginpage():
+def disp_login():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
     print(app)
@@ -20,7 +23,13 @@ def disp_loginpage():
     print(request.args)
     print("***DIAG: request.headers ***")
     print(request.headers)
-    return render_template('login.html')
+    if 'user' in session:
+        user = session['user']
+        loadText = "Click the button below to logout."
+        return render_template("response.html", username=user, request_method=request.method, message=loadText)
+    else:
+        landingText = "Enter your username below to proceed."
+        return render_template('login.html', message=landingText)
 
 
 @app.route("/auth")
@@ -34,23 +43,42 @@ def response():
     print(request.args)
     print("***DIAG: request.headers ***")
     print(request.headers)
-    user0 = 'kevinjonleafy'
-    passw0 = 'dellthebestlaptop15'
-    error = ""
+    user0 = 'a'
+    passw0 = 'b'
+    loadText = ""
     user = ""
     passw = ""
-    if request.method == 'GET':
-        user = request.args['username']
-        passw = request.args['password']
-    else request.method == 'POST':
-        user = request.form['username']
-        passw = request.form['password']
-    if user0 != user and passw0 != passw:
-        error = "uer and pass wrong"
-    try:
-        return render_template("response.html", username=user, password=passw)
-    except:
-        return render_template("login.html")
+    if 'user' in session:
+        user = session['user']
+        loadText = "Click the button below to logout."
+        return render_template("response.html", username=user, request_method=request.method, message=loadText)
+    else:
+        if request.method == 'GET':
+            user = request.args['username']
+            passw = request.args['password']
+        elif request.method == 'POST':
+            user = request.form['username']
+            passw = request.form['password']
+        if user0 != user and passw0 != passw:
+            loadText = "Could not log in. User and password wrong."
+            return render_template("login.html", message=loadText)
+        elif user0 != user and passw0 == passw:
+            loadText = "Could not log in. User is wrong."
+            return render_template("login.html", message=loadText)
+        elif user0 == user and passw0 != passw:
+            loadText = "Could not log in. Password is wrong."
+            return render_template("login.html", message=loadText)
+        else:
+            session['user'] = user
+            loadText = "Click the button below to logout."
+            return render_template("response.html", username=user, request_method=request.method, message=loadText)
+
+
+@app.route("/logout")
+def logoutSession():
+    landingText = "You've logged out. Enter your username below to proceed. "
+    session.pop('user', None)
+    return render_template("login.html", message=landingText)
 
 
 if __name__ == "__main__":
