c6086d9342c310e587e29f1d762560464cc7fc62
kcao20 <kcao2022@gmail.com>
2022-02-04 09:23:19 -0500

    add k27

---
+++ 27_js/fibfac.js
@@ -0,0 +1,29 @@
+// The Adjective Nouns :: Noakai Aronesty, Kevin Cao
+// SoftDev pd2
+// K27 -- Basic functions in JavaScript
+// 2022-02-04
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T
+
+function fact(n) {
+  if (n > 2) {
+    return fact(n - 1) * n
+  } else {
+    return n
+  }
+}
+
+function fib(n) {
+  if (n < 2) {
+    return n
+  } else {
+    return fib(n - 1) + fib(n - 2)
+  }
+}
